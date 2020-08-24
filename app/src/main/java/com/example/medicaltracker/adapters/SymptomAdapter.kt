7cf253971b4d76d39efe266695aca000b8ac4e56
gregory rubenstein <gregoryrubenstein@gmail.com>
2020-08-24 13:58:08 -0400

    updated viewholder

--- app/src/main/java/com/example/medicaltracker/adapters/SymptomAdapter.kt
+++ app/src/main/java/com/example/medicaltracker/adapters/SymptomAdapter.kt
@@ -1,15 +1,14 @@
 package com.example.medicaltracker.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.medicaltracker.R
 import com.example.medicaltracker.data.Symptom
-import com.example.medicaltracker.utilities.util
 
-class SymptomAdapter : RecyclerView.Adapter<util.TextItemViewHolder>() {
-
+class SymptomAdapter : RecyclerView.Adapter<SymptomAdapter.ViewHolder>() {
     var data = listOf<Symptom>()
         set(value) {
             field = value
@@ -20,14 +19,32 @@ class SymptomAdapter : RecyclerView.Adapter<util.TextItemViewHolder>() {
         return data.size
     }
 
-    override fun onBindViewHolder(holder: util.TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.toString()
+        holder.bind(item)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): util.TextItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return util.TextItemViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
     }
+
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val symptomName: TextView = itemView.findViewById(R.id.symptom_name)
+        val symptomTime: TextView = itemView.findViewById(R.id.symptom_date)
+//        val symptomStrength: TextView = itemView.findViewById(R.id.symptom_strength)
+
+        fun bind(item: Symptom) {
+            symptomName.text = item.name
+            symptomTime.text = item.time
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.symptom_item_view, parent, false)
+                return ViewHolder(view)
+            }
+        }
+    }
+
 }
